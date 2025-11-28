fd2771855d0fe6564e1dbf1ef854f2b8581a59c7
ucup233 <sufhidayat9@gmail.com>
2025-11-28 18:47:13 +0700

    Pembuatan Home view model

---
+++ app/src/main/java/com/example/week9/viewmodel/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.week9.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.week9.repositori.RepositoriSiswa
+import com.example.week9.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeViewModel: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+}
