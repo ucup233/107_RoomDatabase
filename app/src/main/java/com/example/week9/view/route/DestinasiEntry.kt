aa1c82004a07faca06e2878aeeae0c7d18d05bbe
ucup233 <sufhidayat9@gmail.com>
2025-11-28 18:29:19 +0700

    Pembuatan file Destinasi Entry

---
+++ app/src/main/java/com/example/week9/view/route/DestinasiEntry.kt
@@ -0,0 +1,10 @@
+package com.example.week9.view.route
+
+import com.example.week9.R
+
+object DestinasiEntry: DestinasiNavigasi {
+
+    override val route = "item_entry"
+
+    override val titleRes = R.string.entry_siswa
+}
