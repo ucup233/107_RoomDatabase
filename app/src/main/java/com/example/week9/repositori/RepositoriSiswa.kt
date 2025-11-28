8ae376f483bbcba60532e7603396f53da58328af
ucup233 <sufhidayat9@gmail.com>
2025-11-28 10:07:49 +0700

    Pembuatan repositori Siswa

---
+++ app/src/main/java/com/example/week9/repositori/RepositoriSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.week9.repositori
+
+import com.example.week9.room.Siswa
+import com.example.week9.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa{
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
