3f95abee9ac19abd3068de7465cee78b79a7ea3f
ucup233 <sufhidayat9@gmail.com>
2025-11-28 18:30:04 +0700

    Pembuatan file Destinasi Home

---
+++ app/src/main/java/com/example/week9/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.week9.view.route
+
+import com.example.week9.R
+
+object DestinasiHome: DestinasiNavigasi {
+
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
