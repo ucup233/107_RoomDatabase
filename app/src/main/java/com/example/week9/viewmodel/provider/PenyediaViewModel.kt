59d79490a698a4ce7479e1df973ae125934dda73
ucup233 <sufhidayat9@gmail.com>
2025-11-28 18:50:01 +0700

    Pembuatan file provider penyedia view model

---
+++ app/src/main/java/com/example/week9/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.week9.viewmodel.provider
+
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.week9.viewmodel.EntryViewModel
+import com.example.week9.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory{
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory
+        .APPLICATION_KEY] as AplikasiSiswa)
