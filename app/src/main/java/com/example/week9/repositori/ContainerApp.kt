7f0691415b95e0bbbd7d7bc774894518500376b3
ucup233 <sufhidayat9@gmail.com>
2025-11-28 19:49:15 +0700

    Perbaikan kode container app file

--- app/src/main/java/com/example/week9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/week9/repositori/ContainerApp.kt
@@ -1,16 +1,26 @@
 package com.example.week9.repositori
 
+import android.app.Application
 import android.content.Context
 import com.example.week9.room.DatabaseSiswa
 
-interface ContainerApp{
-    val repositoriSiswa : RepositoriSiswa
+interface ContainerApp {
+    val repositorySiswa : RepositoriSiswa
 }
 
 class ContainerDataApp(private val context: Context):
-    ContainerApp {
-        override val repositoriSiswa: RepositoriSiswa by lazy {
+    ContainerApp{
+    override val repositorySiswa: RepositoriSiswa by lazy {
         OfflineRepositoriSiswa(
-            DatabaseSiswa.getDatabase(context).siswaDao())
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context=this)
     }
 }
