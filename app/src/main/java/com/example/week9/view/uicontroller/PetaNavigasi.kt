0ca15074cd74c2748f7682ff344cd1f1009e9dad
ucup233 <sufhidayat9@gmail.com>
2025-11-28 18:31:45 +0700

    Pembuatan file Peta Navigasi untuk navigasi

---
+++ app/src/main/java/com/example/week9/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,31 @@
+package com.example.week9.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+
+    }
+}
