61fcecab3a8573ab1783b87a058454a4f5cdb6e5
ucup233 <sufhidayat9@gmail.com>
2025-12-05 09:40:10 +0700

    Penambahan function get siswa

--- app/src/main/java/com/example/week9/room/SiswaDao.kt
+++ app/src/main/java/com/example/week9/room/SiswaDao.kt
@@ -13,4 +13,7 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query(value = "SELECT * FROM tblSiswa WHERE id = :id")
+    fun getSiswa(id : Int): Flow<Siswa?>
 }
