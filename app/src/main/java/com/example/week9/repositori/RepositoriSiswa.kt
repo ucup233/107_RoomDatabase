2a7efd302ff06e6f988b8e5521eaf60133642cc4
ucup233 <sufhidayat9@gmail.com>
2025-12-05 10:12:22 +0700

    Penambahan function untuk update siswa di repositori siswa

--- app/src/main/java/com/example/week9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/week9/repositori/RepositoriSiswa.kt
@@ -9,6 +9,7 @@ interface RepositoriSiswa{
     suspend fun insertSiswa(siswa: Siswa)
     fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -18,4 +19,5 @@ class OfflineRepositoriSiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
