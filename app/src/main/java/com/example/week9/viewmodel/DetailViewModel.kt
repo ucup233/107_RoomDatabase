a53885dd4c34760cb51494ff3a47ede39677c45e
ucup233 <sufhidayat9@gmail.com>
2025-12-05 09:56:17 +0700

    Penamnbahan file Detail view model

--- app/src/main/java/com/example/week9/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/week9/viewmodel/DetailViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.week9.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.week9.repositori.RepositoriSiswa
+import com.example.week9.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
-    private val repositoriSiswa: RepositoriSiswa) : ViewModel(){
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel(){
 
     private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
 
@@ -26,6 +36,7 @@ class DetailViewModel (
         private const val TIMEOUT_MILLIS = 5_000L
     }
 }
+
 
 /**
  * UI state for ItemDetailsScreen
