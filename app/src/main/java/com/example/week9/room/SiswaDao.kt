183bcfe42f52b7ec4092ff49b51f23d6e9a061ea
ucup233 <sufhidayat9@gmail.com>
2025-12-05 10:13:05 +0700

    Penambahan update di siswa dao

--- app/src/main/java/com/example/week9/room/SiswaDao.kt
+++ app/src/main/java/com/example/week9/room/SiswaDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -20,4 +21,7 @@ interface SiswaDao {
 
     @Delete
     suspend fun delete(siswa: Siswa)
+
+    @Update
+    suspend fun update(siswa: Siswa)
 }
